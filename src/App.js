f413628e76a1b197f6b380def272d1e5ce5f556d
OlfiDell <bobr.rozrabiaka123@gmail.com>
2022-03-24 11:07:35 +0100

    react router added, basic configuration applied

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import FullData from "./components/FullData/FullData";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <FullData />
+        <Routes>
+          <Route path="/directories" exact element={<FullData />}></Route>
+          {/* <Route
+            path="/directories/:directoryCode"
+            element={<SingleData></SingleData>}
+          ></Route> */}
+          <Route path="*" element={<Navigate to="/directories" />}></Route>
+        </Routes>
       </header>
     </div>
   );
