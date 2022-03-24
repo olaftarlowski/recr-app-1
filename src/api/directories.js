eafd401ef7a524ea3f04cf8cb8cda087571f8055
OlfiDell <bobr.rozrabiaka123@gmail.com>
2022-03-24 10:40:59 +0100

    api call updated, CRA clean up applied

---
+++ src/api/directories.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://fnp5vd20r2.execute-api.us-east-1.amazonaws.com/dev",
+});
--- src/components/FullData/FullData.js
+++ src/components/FullData/FullData.js
@@ -1,48 +1,73 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import api from "../../api/directories";
 
 const FullData = () => {
-  const [fullData, setFullData] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [isError, setIsError] = useState(null);
+  const [fullData, setFullData] = useState();
 
   useEffect(() => {
-    axios
-      .get(
-        "https://fnp5vd20r2.execute-api.us-east-1.amazonaws.com/dev/directories"
-      )
-      .then((res) => {
-        const items = res.data;
-        console.log(items);
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/directories");
+        setFullData(response.data);
         setIsLoaded(true);
-        setFullData(items);
-      })
-      .catch((error) => {
-        setIsLoaded(true);
-        setIsError(error);
-      });
+      } catch (err) {
+        setIsError(err);
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    };
+
+    fetchData();
   }, []);
+
   console.log(fullData);
 
   if (isError) {
-    return <div>{isError}</div>;
+    return <div>{isError.message}</div>;
   }
 
   if (!isLoaded) {
     return <div>LOADING</div>;
-  }
-
-  if (fullData) {
-    let content = fullData.files[7].name;
+  } else if (fullData && !isError) {
+    let directiories = fullData.directories.map((dirItem) => {
+      return (
+        <div
+          style={{ width: "100px", height: "100px", border: "1px solid red" }}
+        >
+          <p>{dirItem.name}</p>
+          <p>{dirItem.id}</p>
+        </div>
+      );
+    });
+    let files = fullData.files.map((fileItem) => {
+      return (
+        <div
+          style={{ width: "100px", height: "100px", border: "1px solid red" }}
+        >
+          <p>{fileItem.name}</p>
+          <p>{fileItem.id}</p>
+        </div>
+      );
+    });
     return (
       <div>
         <p>data z api</p>
-        <p>{content}</p>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {directiories}
+          {files}
+        </div>
       </div>
     );
+  } else {
+    return <div>Bad Request</div>;
   }
-
-  return <div>Bad Request</div>;
 };
 
 export default FullData;
